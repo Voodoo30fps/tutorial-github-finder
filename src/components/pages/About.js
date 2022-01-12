246413ca4fbc965cb11bc1efe44aa0d1a11c8cac
Voodoo30fps <voodoo30fps@gmail.com>
2022-01-12 09:06:23 +0200

    WITHOUT hooks and context

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react/cjs/react.development';
+import React from 'react';
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>About This App</h1>
+            <p>App to search GitHub users</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>
+    );
+};
+
+export default About;
